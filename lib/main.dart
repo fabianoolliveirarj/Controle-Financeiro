2814df9cecc3187dc9c7619d525615763c9fb500
fabianoolliveirarj <fabianobento2017@gmail.com>
2025-04-21 22:32:41 -0300

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:controle_financeiro/pages/home_page.dart';
+
+void main() {
+  runApp(const ControleFinanceiroApp());
+}
+
+class ControleFinanceiroApp extends StatelessWidget {
+  const ControleFinanceiroApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Controle Financeiro',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
